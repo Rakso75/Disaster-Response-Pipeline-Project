6bcb96d9b9ef61084a984edbed8b6cf76c52127f
Rakso75 <sven.lothwesen@gmail.com>
2022-12-26 07:40:59 +0000

    Save DB tablename fix

--- data/process_data.py
+++ data/process_data.py
@@ -1,4 +1,6 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
@@ -13,9 +15,8 @@ def load_data(messages_filepath, categories_filepath):
     
     """
     
-    messages = pd.read_csv('./messages.csv')
-    categories = pd.read_csv('./categories.csv')
-    # merge datasets
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
     df = messages.merge(categories, on='id')
     return df
 
@@ -29,42 +30,56 @@ def clean_data(df):
     df - Cleaned data
     """
 
+#     # remove 'child_alone' column as it has only 0 (ZERO) values. => no value for classification and prediction
+#     df = df.drop('child_alone', axis = 1)
+
+#     # Value '2' in 'related' column is causing problems later on in the ML pipeline(see above).
+#     # value '2' should be replaced with '0' or '1'. By majority vote it will be replace with '1'.
+#     df['related'] = df['related'].map(lambda x: 1 if x==2 else x) 
+
     # create a dataframe of the 36 individual category columns
     categories = df.categories.str.split(pat=';', expand=True)
     
     # use the first row to extract a list of new column names for categories.
-row = categories.iloc[0]
+    row = categories.iloc[0]
 # one way is to apply a lambda function that takes everything 
 # up to the second to last character of each string with slicing
-category_colnames = row.transform(lambda x: x[:-2]).tolist()
+    category_colnames = row.transform(lambda x: x[:-2]).tolist()
 # rename the columns of `categories`
-categories.columns = category_colnames
+    categories.columns = category_colnames
 
-for column in categories:
-    # set each value to be the last character of the string
-    categories[column] = categories[column].astype(str).str[-1]
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
     
-    # convert column from string to numeric
-    categories[column] = categories[column].astype(int)
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
 
-# drop the original categories column from `df`
-df.drop(columns=['categories'], inplace=True)
-# drop duplicates
-df.drop_duplicates(inplace=True)
-# remove 'child_alone' column as it has only 0 (ZERO) values. => no value for classification and prediction
-df = df.drop('child_alone', axis = 1)
+    # drop the original categories column from `df`
+    df.drop(columns=['categories'], inplace=True)
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
 
-# Value '2' in 'related' column is causing problems later on in the ML pipeline(see above).
-# value '2' should be replaced with '0' or '1'. By majority vote it will be replace with '1'.
-df['related'] = df['related'].map(lambda x: 1 if x==2 else x) 
-
-return df
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
 
+    """
+    INPUT:
+    df - cleaned data
+    database_filename - database filename for sqlite database with (.db) file type
+    
+    OUTPUT:
+    None - saves cleaned data into sqlite database
+    
+    """
+    
+    engine = create_engine('sqlite:///' + database_filename)
+    table_name = database_filename.replace(".db","") + "_table"
+    df.to_sql(table_name, engine, index=False, if_exists='replace') # , if_exists='replace' ##REMOVE##
 
+    
 def main():
     if len(sys.argv) == 4:
 
