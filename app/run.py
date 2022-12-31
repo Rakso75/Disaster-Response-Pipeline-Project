6427cb7d409b93d92c2ead7ce8a8dd598c503e63
Rakso75 <sven.lothwesen@gmail.com>
2022-12-31 13:46:54 +0000

    Update run.py

--- app/run.py
+++ app/run.py
@@ -27,10 +27,10 @@ def tokenize(text):
 
 # load data
 engine = create_engine('sqlite:///../data/ETL_Preparation.db')
-df = pd.read_sql_table('ETL_Preparation', engine)
+df = pd.read_sql_table('ETL_Preparation_table', engine)
 
 # load model
-model = joblib.load("../models/cv_gridsearch_result.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
